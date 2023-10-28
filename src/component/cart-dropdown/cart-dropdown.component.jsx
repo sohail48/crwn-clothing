5291b8734daed16470673f142402abe15abf71ef
sohail48 <113433385+sohail48@users.noreply.github.com>
2023-10-28 15:54:11 +0530

    Fixing name.

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Button from '../button/button.component';
+import { useContext } from 'react';
+import {CartContext} from '../../contexts/cart.context';
 import '../cart-dropdown/cart-dropdown.styles.scss';
+import CartItem from '../cart item/cart-item.component';
 
-const CartDropdown = () => (
-    <div className='cart-dropdown-container'>
-        <div className='cart-items'/>
-        <Button>CHECKOUT</Button>
-    </div>
-);
-    
-        
-    
-
+const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext)
+    return(
+        <div className='cart-dropdown-container'>
+            <div className='cart-items'>
+                {cartItems.map((item) => (
+                    <CartItem key={item.id} cartItem={item} />
+                ))}
+            </div>
+            <Button>CHECKOUT</Button>
+        </div>
+    );
+};
 
 export default CartDropdown;
