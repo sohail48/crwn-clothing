0e07c146448753ad2a5f5f09ae385c0cdf4f28e8
sohail48 <113433385+sohail48@users.noreply.github.com>
2023-11-10 19:31:42 +0530

    Fixing cart-dropdown selectors

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState, useEffect} from 'react';
+import {createContext, useEffect, useReducer} from 'react';
 
 import {onAuthStateChangedListener, createUserDocumentFromAuth} from '../utils/firebase/firebase.utils';
 
@@ -7,10 +7,32 @@ export const UserContext = createContext({
     currentUser: null,
 });
 
+
+export const USER_ACTION_TYPES = {
+    SET_CURRECT_USER: 'SET_CURRECT_USER',
+};
+
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
+const userReducer = (state, action) => {
+    const {type, payload} = action;
+
+    switch(type){
+        case  USER_ACTION_TYPES.SET_CURRECT_USER:
+            return{...state, currentUser: payload};
+        default:
+            throw new Error(`unhandled type ${type} in userReducer`);
+    }
+};
+
 export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = {currentUser, setCurrentUser};
+    const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
     
+    const setCurrentUser = (user) => 
+        dispatch({type:  USER_ACTION_TYPES.SET_CURRECT_USER, currentUser: user});
+     
     useEffect(() => {
         const unsubcribe = onAuthStateChangedListener((user) => {
             if(user) {
@@ -21,6 +43,10 @@ export const UserProvider = ({children}) => {
 
         return unsubcribe;
     }, []);
+
+    const value = {
+        currentUser,
+    }
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
